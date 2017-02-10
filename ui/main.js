5988244649ba8a1ef8ef35fa9a7771a2d3314c8b
bharathsridhar72 <bharathsridhar72@gmail.com>
2017-02-10 10:27:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 // ,ove the image
-var id = document.getElement('madi');
+var id = document.getElementById('madi');
 madi.onclick = function() {
     madi.style.margin(inLeft='100px');
 };
