1fe95b18c2d212f31fae38828ef75b31d2ef762f
bharathsridhar72 <bharathsridhar72@gmail.com>
2017-02-10 10:39:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,13 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
 // ,ove the image
+var marginLeft = 0;
 var id = document.getElementById('madi');
+function moveRight(){
+    marginLeft = MarginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    }
 madi.onclick = function() {
+    var interval = setInterval(moveRight,100);
     madi.style.marginLeft = '100px';
 };
