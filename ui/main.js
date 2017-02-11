7d23e15c3caa71afa105ea637f0d73396e36b802
bharathsridhar72 <bharathsridhar72@gmail.com>
2017-02-11 22:11:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-    //make a  request to the counter endpoint
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString()
-    //capture the response
-}
+    //create a  request to the counter endpoint
+    var request = XMLhttpsrequest();
+    //Captre the response and store it in a variable
+    request.onreadystatechange =  function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status = 200){
+                var couter = request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString()
+            }
+        }
+    };
+  
+   
+    //Make a response
+    request.open('GET','http://bharathsridhar72.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
